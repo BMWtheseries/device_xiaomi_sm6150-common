823186bd4eaafcc3926e80212f261cf6cc319278
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    sm6150-common: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Stk Photos DevicePersonalizationPrebuiltPixel2021 GoogleRestorePrebuilt
+LOCAL_OVERRIDES_PACKAGES += FM2 DevicePolicyPrebuilt AndroidAutoStubPrebuilt PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt ScribePrebuilt TurboPrebuilt WellbeingPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
