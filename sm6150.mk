d79fa0a07d087f6b695cc117d9ccdd67785427e3
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-06-13 11:10:01 +0700

    sm6150-common: AncientOS
    Change-Id: I4b4fe0ef2004cbd86c4b927f0f3de27ffbff3820

--- sm6150.mk
+++ sm6150.mk
@@ -25,7 +25,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ancient
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
